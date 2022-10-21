4ba954e05a4cc49e2be5b480f10e9b23139446ad
LenaBystrykh <lena.bystryh@mail.ru>
2022-10-21 16:12:40 +0500

    updated about page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -3,12 +3,10 @@
     <div class="md-body">
         <ul>
             <li>Vue CLI template (created earlier) init</li>
-            <li>VUEX setting</li>
             <li>Notes saved in localStorage</li>
-            <li>Data transfer between components</li>
-            <li>Separation of logic between components</li>
-            <li>Items filtration</li>
-            <li>Global configs created</li>
+            <li>Data transfer between components (props)</li>
+            <li>Separation of logic between components (emits)</li>
+            <li>Note deletion and editing (v-if, events)</li>
         </ul>
     </div>
 </template>
